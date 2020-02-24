755b9845bf5bd353d661b495c7d7ff5c07c13a7c
Molnár Gábor <gabor.molnar@icellmobilsoft.hu>
2020-02-24 17:16:56 +0100

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hcn!pyfaul-$3bo^piw^id$#p)d_lzbdgh+gwba@29&v-y4ung'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'gabormolnar.pythonanywhere.com']
 
 
 # Application definition
